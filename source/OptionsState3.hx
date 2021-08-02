e521de85b01799160fc2dd934054e8fbbc709fdd
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-08-02 21:25:06 +0300

    update OptionsState3.hx

--- source/OptionsState3.hx
+++ source/OptionsState3.hx
@@ -89,6 +89,7 @@ class OptionsState3 extends MusicBeatState
 		notice.screenCenter();
 		notice.y = FlxG.height - 56;
 		notice.alpha = 0.6;
+		notice.alignment = CENTER;
 		add(noticebg);
 		add(notice);
 		#if (android || ios)
