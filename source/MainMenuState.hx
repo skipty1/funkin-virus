1923d177bf7ca067132ab2f37269825162424149
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-08-14 09:50:11 +0300

    update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -61,6 +61,14 @@ class MainMenuState extends MusicBeatState
 	public var Achievement:FlxSprite;
 	public var Music:FlxSprite;
 	public var Bestplayer:FlxSprite;
+	public var Freeplay:FlxSprite;
+	public var Gold:FlxSprite;
+	public var Install:FlxSprite;
+	public var Iron:FlxSprite;
+	public var Rainbow:FlxSprite;
+	public var Story:FlxSprite;
+	public var Xshit:Float = 0;
+	public var Yshit:Float = 0;
 
 	override function create()
 	{
@@ -155,7 +163,74 @@ class MainMenuState extends MusicBeatState
 		else
 			Bestplayer.color = FlxColor.fromHSL(Bestplayer.color.hue, Bestplayer.color.saturation, 0.7, 1);
 
+		Freeplay = new FlxSprite(Xshit,Yshit);
+		Freeplay.frames = Paths.getSparrowAtlas('8bit/MUNE','shared');;
+		Freeplay.animation.addByPrefix('selected','freepaly',24,false);
+		Freeplay.animation.addByPrefix('unselected','unfreeplay',24,false);
+		Freeplay.animation.addByPrefix('clicked','tapfreeplay0',24,false);
+		Freeplay.scale.set(2,2);
+		Freeplay.antialiasing = false;
+		Freeplay.animation.play('unselected');
+		add(Freeplay);
+		if (FlxG.save.data.storyBeated)
+			Freeplay.color = FlxColor.fromHSL(Freeplay.color.hue, Freeplay.color.saturation, 1, 1);
+		else
+			Freeplay.color = FlxColor.fromHSL(Freeplay.color.hue, Freeplay.color.saturation, 0.7, 1);
 
+		Gold = new FlxSprite(Xshit,Yshit);
+		Gold.frames = Paths.getSparrowAtlas('8bit/MUNE','shared');;
+		Gold.animation.addByPrefix('mmmhi','gold0',24,false);
+		Gold.scale.set(2,2);
+		Gold.antialiasing = false;
+		Gold.animation.play('mmmhi');
+		add(Gold);
+		if (FlxG.save.data.GoldTrophy)
+			Gold.color = FlxColor.fromHSL(Gold.color.hue, Gold.color.saturation, 1, 1);
+		else
+			Gold.color = FlxColor.fromHSL(Gold.color.hue, Gold.color.saturation, 0.7, 1);
+
+		Install = new FlxSprite(Xshit,Yshit);
+		Install.frames = Paths.getSparrowAtlas('8bit/MUNE','shared');;
+		Install.animation.addByPrefix('selected','install0',24,false);
+		Install.animation.addByPrefix('unselected','uninstall0',24,false);
+		Install.scale.set(2,2);
+		Install.antialiasing = false;
+		Install.animation.play('unselected');
+		add(Install);
+
+		Iron = new FlxSprite(Xshit,Yshit);
+		Iron.frames = Paths.getSparrowAtlas('8bit/MUNE','shared');;
+		Iron.animation.addByPrefix('mmmhi','iron0',24,false);
+		Iron.scale.set(2,2);
+		Iron.antialiasing = false;
+		Iron.animation.play('unselected');
+		add(Iron);
+		if (FlxG.save.data.IronTrophy)
+			Iron.color = FlxColor.fromHSL(Iron.color.hue, Iron.color.saturation, 1, 1);
+		else
+			Iron.color = FlxColor.fromHSL(Iron.color.hue, Iron.color.saturation, 0.7, 1);
+
+		Rainbow = new FlxSprite(Xshit,Yshit);
+		Rainbow.frames = Paths.getSparrowAtlas('8bit/MUNE','shared');;
+		Rainbow.animation.addByPrefix('mmmhi','rainbow0',24,false);
+		Rainbow.scale.set(2,2);
+		Rainbow.antialiasing = false;
+		Rainbow.animation.play('unselected');
+		add(Rainbow);
+		if (FlxG.save.data.RainbowTrophy)
+			Rainbow.color = FlxColor.fromHSL(Rainbow.color.hue, Rainbow.color.saturation, 1, 1);
+		else
+			Rainbow.color = FlxColor.fromHSL(Rainbow.color.hue, Rainbow.color.saturation, 0.7, 1);
+
+		Story = new FlxSprite(Xshit,Yshit);
+		Story.frames = Paths.getSparrowAtlas('8bit/MUNE','shared');;
+		Story.animation.addByPrefix('selected','story0',24,false);
+		Story.animation.addByPrefix('unselected','unstory0',24,false);
+		Story.animation.addByPrefix('clicked','tapstory0',24,false);
+		Story.scale.set(2,2);
+		Story.antialiasing = false;
+		Story.animation.play('unselected');
+		add(Story);
 
 		var madvirus:FlxSprite = new FlxSprite(-100).loadGraphic(Paths.image('8bit/MVAT','shared'));
 		madvirus.scrollFactor.x = 0;
@@ -211,70 +286,70 @@ class MainMenuState extends MusicBeatState
 			if (FlxG.mouse.overlaps(spike) && FlxG.mouse.justPressed && ASS == 'SAND' && !FlxG.save.data.Spike)
 				medalPop('Spike');
 //animation system for overlaps
-			if (FlxG.mouse.overlaps(stupidItems.Achievement)){
-				stupidItems.playAnimation(4, 'selected');
+			if (FlxG.mouse.overlaps(Achievement)){
+				playAnimation(4, 'selected');
 			}else{
-				stupidItems.playAnimation(4, 'unselected');
+				playAnimation(4, 'unselected');
 			}
 
-			if (FlxG.mouse.overlaps(stupidItems.Music)){
-				stupidItems.playAnimation(3, 'selected');
+			if (FlxG.mouse.overlaps(Music)){
+				playAnimation(3, 'selected');
 			}else{
-				stupidItems.playAnimation(3, 'unselected');
+				playAnimation(3, 'unselected');
 			}
 
-			if (FlxG.mouse.overlaps(stupidItems.Freeplay)){
-				stupidItems.playAnimation(2, 'selected');
+			if (FlxG.mouse.overlaps(Freeplay)){
+				playAnimation(2, 'selected');
 			}else{
-				stupidItems.playAnimation(2, 'unselected');
+				playAnimation(2, 'unselected');
 			}
 
-			if (FlxG.mouse.overlaps(stupidItems.Install)){
-				stupidItems.playAnimation(1, 'selected');
+			if (FlxG.mouse.overlaps(Install)){
+				playAnimation(1, 'selected');
 			}else{
-				stupidItems.playAnimation(1, 'unselected');
+				playAnimation(1, 'unselected');
 			}
 
-			if (FlxG.mouse.overlaps(stupidItems.Story)){
-				stupidItems.playAnimation(0, 'selected');
+			if (FlxG.mouse.overlaps(Story)){
+				playAnimation(0, 'selected');
 			}else{
-				stupidItems.playAnimation(0, 'unselected');
+				playAnimation(0, 'unselected');
 			}
 //select system
-			if (FlxG.mouse.overlaps(stupidItems.Story) && FlxG.mouse.justPressed){
-				stupidItems.playAnimation(0, 'clicked');
+			if (FlxG.mouse.overlaps(Story) && FlxG.mouse.justPressed){
+				playAnimation(0, 'clicked');
 				FlxG.sound.play(Paths.sound('confirmMenu'));
 				selectedSomethin = true;
 				FlxG.mouse.visible = false;
 				FlxG.switchState(new TitleState());
 			}
 
-			if (FlxG.mouse.overlaps(stupidItems.Freeplay) && FlxG.mouse.justPressed && FlxG.save.data.storyBeated){
-				stupidItems.playAnimation(2, 'clicked');
+			if (FlxG.mouse.overlaps(Freeplay) && FlxG.mouse.justPressed && FlxG.save.data.storyBeated){
+				playAnimation(2, 'clicked');
 				FlxG.sound.play(Paths.sound('confirmMenu'));
 				selectedSomethin = true;
 				FlxG.mouse.visible = false;
 				FlxG.switchState(new FreeplayState());
 			}
 
-			if (FlxG.mouse.overlaps(stupidItems.Install) && FlxG.mouse.justPressed){
-				//stupidItems.play(0, 'clicked');
+			if (FlxG.mouse.overlaps(Install) && FlxG.mouse.justPressed){
+				//play(0, 'clicked');
 				FlxG.sound.play(Paths.sound('confirmMenu'));
 				selectedSomethin = true;
 				FlxG.mouse.visible = false;
 				FlxG.switchState(new OptionsMenu());
 			}
 
-			if (FlxG.mouse.overlaps(stupidItems.Music) && FlxG.mouse.justPressed){
-				//stupidItems.play(0, 'clicked');
+			if (FlxG.mouse.overlaps(Music) && FlxG.mouse.justPressed){
+				//play(0, 'clicked');
 				FlxG.sound.play(Paths.sound('confirmMenu'));
 				selectedSomethin = true;
 				FlxG.mouse.visible = false;
 				FlxG.switchState(new TapeState());
 			}
 
-			if (FlxG.mouse.overlaps(stupidItems.Achievement) && FlxG.mouse.justPressed){
-				//stupidItems.play(0, 'clicked');
+			if (FlxG.mouse.overlaps(Achievement) && FlxG.mouse.justPressed){
+				//play(0, 'clicked');
 				FlxG.sound.play(Paths.sound('confirmMenu'));
 				selectedSomethin = true;
 				FlxG.mouse.visible = false;
@@ -292,5 +367,21 @@ class MainMenuState extends MusicBeatState
 		else
 			super.update(elapsed);
 
+	}
+	public function playAnimation(ID:Int,Anim:String){
+		switch (ID){
+			case 0:
+				Story.animation.play(Anim);
+			case 1:
+				Install.animation.play(Anim);
+			case 2:
+				Freeplay.animation.play(Anim);
+			case 3:
+				Music.animation.play(Anim);
+			case 4:
+				Achievement.animation.play(Anim);
+			default:
+				trace('No valid animation');
+		}
 	}
 }
