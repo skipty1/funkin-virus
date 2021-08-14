d19c505fd08aca2a585537d661242ba5adb17fd0
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-08-14 09:43:41 +0300

    update MenuThings.hx

--- source/MenuThings.hx
+++ source/MenuThings.hx
@@ -18,10 +18,10 @@ using StringTools;
 
 class MenuThings extends FlxSpriteGroup{
 	//varrrsss
-	public var Achievement:FlxSprite;
-	public var Music:FlxSprite;
-	public var Bestplayer:FlxSprite;
-	public var Freeplay:FlxSprite;
+
+
+
+
 	public var Gold:FlxSprite;
 	public var Install:FlxSprite;
 	public var Iron:FlxSprite;
@@ -37,35 +37,8 @@ class MenuThings extends FlxSpriteGroup{
 		Yshit = offsetY;
 		//pathOne = Paths.getSparrowAtlas('8bit/MUNE','shared');
 
-		Achievement = new FlxSprite(Xshit, Yshit);
-		Achievement.frames = Paths.getSparrowAtlas('8bit/MUNE','shared');;
-		Achievement.animation.addByPrefix('selected','Achievement0',24,false);
-		Achievement.animation.addByPrefix('unselected','unAchievement0',24,false);
-		Achievement.scale.set(2,2);
-		Achievement.antialiasing = false;
-		Achievement.animation.play('unselected');
-		add(Achievement);
 
-		Music = new FlxSprite(Xshit,Yshit);
-		Music.frames = Paths.getSparrowAtlas('8bit/MUNE','shared');;
-		Music.animation.addByPrefix('selected','MUSIC',24,false);
-		Music.animation.addByPrefix('unselected','UNMUSIC',24,false);
-		Music.scale.set(2,2);
-		Music.antialiasing = false;
-		Music.animation.play('unselected');
-		add(Music);
 
-		Bestplayer = new FlxSprite(Xshit,Yshit);
-		Bestplayer.frames = Paths.getSparrowAtlas('8bit/MUNE','shared');;
-		Bestplayer.animation.addByPrefix('mmmhi','best player',24,false);
-		Bestplayer.scale.set(2,2);
-		Bestplayer.antialiasing = false;
-		Bestplayer.animation.play('mmmhi');
-		add(Bestplayer);
-		if (FlxG.save.data.BestTrophy)
-			Bestplayer.color = FlxColor.fromHSL(Bestplayer.color.hue, Bestplayer.color.saturation, 1, 1);
-		else
-			Bestplayer.color = FlxColor.fromHSL(Bestplayer.color.hue, Bestplayer.color.saturation, 0.7, 1);
 
 
 		Freeplay = new FlxSprite(Xshit,Yshit);
