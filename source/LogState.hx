321a6f2c605e43a770239aaa1ca01ad928edcf90
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-09-11 12:10:03 +0300

    update LogState.hx

--- source/LogState.hx
+++ source/LogState.hx
@@ -8,6 +8,7 @@ import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.effects.FlxFlicker;
 import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.addons.ui.FlxUIInputText;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
 import flixel.tweens.FlxEase;
@@ -26,7 +27,11 @@ import Discord.DiscordClient;
 #end
 
 class LogState extends MusicBeatState{
-	
+
+	var chooseName:FlxText;
+
+	var name:FlxUIInputText;
+
 	override public function create(){
 		#if sys
 		#if desktop
@@ -50,5 +55,85 @@ class LogState extends MusicBeatState{
 		MedalSaves.initMedal();
 		
 		Highscore.load();
+		
+		chooseName = new FlxText(FlxG.width * 0.7, 5, 0, "Type in your BetaTester code.", 32);
+		chooseName.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, RIGHT);
+		chooseName.alignment = CENTER;
+		chooseName.setBorderStyle(OUTLINE, 0xFF000000, 5, 1);
+		chooseName.screenCenter(X);
+		chooseName.y = 38;
+		chooseName.scrollFactor.set();
+		add(chooseName);
+
+		name = new FlxUIInputText(10, 10, FlxG.width, '', 8);
+		name.setFormat(Paths.font("vcr.ttf"), 50, FlxColor.WHITE, RIGHT);
+		name.alignment = CENTER;
+		name.setBorderStyle(OUTLINE, 0xFF000000, 5, 1);
+		name.screenCenter();
+		name.scrollFactor.set();
+		add(name);
+		name.backgroundColor = 0xFF000000;
+		name.maxLength = 31;
+		name.lines = 1;
+		name.caretColor = 0xFFFFFFFF;
+
+		doTheFlick();
+
+		super.create();
+	}
+	override public function update(elapsed:Float){
+		super.update(elapsed);
+		
+		name.hasFocus = true;
+		
+		if (FlxG.keys.justPressed.ENTER){
+			switch(name.text.toLowerCase()){
+				case "8bitryan" | "8-bitryan":
+					chooseName.text = "Welcome, Ryan.";
+					moveStates();
+				case "aqua":
+					chooseName.text = "Welcome, Aqua from the MVA Team.";
+					moveStates();
+				case "rusron":
+					chooseName.text = "Welcome, Director.";
+					moveStates();
+				case "john":
+					chooseName.text = "Welcome, John from the MVA Team.";
+					moveStates();
+				case "lucky":
+					chooseName.text = "Welcome, Luckydog from the MVA Team.";
+					moveStates();
+				case "zack":
+					chooseName.text = "Welcome, Zack from the MVA Team.";
+					moveStates();
+				case "tqualizer":
+					chooseName.text = "Welcome, Tea from the MVA Team.";
+					moveStates();
+				case "flippy":
+					chooseName.text = "Welcome, Flippy.";
+					moveStates();
+				case "gegcoin":
+					chooseName.text = "Welcome, Gegcoin.";
+					moveStates();
+				case "ginger":
+					chooseName.text = "Welcome, Ginger from the MVA Team.";
+					moveStates();
+				default:
+					moveStates(true);
+			}
+		}
+	}
+	function moveStates(?lmaoNo:Bool = false){
+		if (!lmaoNo){
+			new FlxTimer().start(1.5, function(tmr:FlxTimer){
+				FlxG.switchState(new MainMenuState());
+			});
+		}
+		else{
+			chooseName.text = "Invalid BetaTester pass.";
+			new FlxTimer().start(1.5, function(tmr:FlxTimer){
+				chooseName.text = "Type in your BetaTester code.";
+			});
+		}
 	}
 }
