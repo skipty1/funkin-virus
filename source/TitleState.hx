43a3a4d7f1f2887bad78346f43f457c38d4541ec
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-09-16 18:55:04 +0300

    update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -196,7 +196,7 @@ class TitleState extends MusicBeatState
 			FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
 
 			FlxG.sound.music.fadeIn(4, 0, 0.7);
-			Conductor.changeBPM(102);
+			Conductor.changeBPM(115);
 			initialized = true;
 		}
 
