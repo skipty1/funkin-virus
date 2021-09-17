451026de7c9f7dcd96bdc0efc2413d41958593b9
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-09-17 19:30:00 +0300

    update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -463,7 +463,7 @@ class MainMenuState extends MusicBeatState
 			black.alpha = 0.4;
 			add(black);
 			onDiffic = true;
-			difficEz = new FlxSprite(-100);
+			difficEz = new FlxSprite();
 			difficEz.frames = Paths.getSparrowAtlas("8bit/Difficulty_selection","shared");
 			difficEz.animation.addByPrefix("selected","EZ");
 			difficEz.animation.addByPrefix("unselected","unEZ");
@@ -471,7 +471,7 @@ class MainMenuState extends MusicBeatState
 			difficEz.animation.play("unselected");
 			difficEz.scale.set(2,2);
 			add(difficEz);
-			difficNor = new FlxSprite(-100);
+			difficNor = new FlxSprite();
 			difficNor.frames = Paths.getSparrowAtlas("8bit/Difficulty_selection","shared");
 			difficNor.animation.addByPrefix("selected","NOM");
 			difficNor.animation.addByPrefix("unselected","unNOM");
@@ -479,7 +479,7 @@ class MainMenuState extends MusicBeatState
 			difficNor.animation.play("unselected");
 			difficNor.scale.set(2,2);
 			add(difficNor);
-			difficHar = new FlxSprite(-100);
+			difficHar = new FlxSprite();
 			difficHar.frames = Paths.getSparrowAtlas("8bit/Difficulty_selection","shared");
 			difficHar.animation.addByPrefix("selected","HARD");
 			difficHar.animation.addByPrefix("unselected","unHARD");
