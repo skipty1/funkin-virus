cccd823274a6b8241933b590402e7fce4e6c7f5e
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-09-23 20:14:13 +0300

    update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -166,6 +166,7 @@ class MainMenuState extends MusicBeatState
 		Music.antialiasing = false;
 		Music.animation.play('unselected');
 		add(Music);
+		Music.color = FlxColor.fromHSL(Music.color.hue, Music.color.saturation, 0.1, 1);
 
 		cloud = new FlxSprite(Music.x - 350, 730);
 		cloud.frames = Paths.getSparrowAtlas('8bit/clouds','shared');
@@ -383,13 +384,13 @@ class MainMenuState extends MusicBeatState
 				FlxG.switchState(new OptionsMenu());
 			}
 
-			if (FlxG.mouse.overlaps(hitboxMusic) && FlxG.mouse.justPressed){
+			/*if (FlxG.mouse.overlaps(hitboxMusic) && FlxG.mouse.justPressed){
 				//play(0, 'clicked');
 				FlxG.sound.play(Paths.sound('confirmMenu'));
 				selectedSomethin = true;
 				FlxG.mouse.visible = false;
 				FlxG.switchState(new TapeState());
-			}
+			}*/
 
 			if (FlxG.mouse.overlaps(hitboxAchievement) && FlxG.mouse.justPressed){
 				//play(0, 'clicked');
