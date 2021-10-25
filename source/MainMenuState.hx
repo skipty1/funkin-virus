7985472e6616c310c507ed7fe2c551c9299b1773
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-10-25 19:25:43 +0300

    update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -309,8 +309,8 @@ class MainMenuState extends MusicBeatState
 	{
 		if (!selectedSomethin)
 		{
-			//if (FlxG.keys.justPressed.A && FlxGameJolt._initialized)
-				//FlxGameJolt.setData("BetaTester?", "true", false);
+			if (FlxG.keys.justPressed.A)
+				FlxG.switchState(new BattleTest());
 
 			if (spike != null && ASS == 'SAND'){
 				if (FlxG.mouse.overlaps(spike) && FlxG.mouse.justPressed && ASS == 'SAND' && !FlxG.save.data.Spike)
